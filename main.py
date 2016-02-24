2b3200fb505bb87585f268419cd4430c64bf3841
brownhead <johnsullivan.pem@gmail.com>
2016-02-24 15:38:05 -0800

    Detect idleness in PRs.
    Test Plan:
    It's complicated :(. Gotta test each case individually.

--- main.py
+++ main.py
@@ -48,14 +48,14 @@ def convert_date_time(date_time_string):
     return datetime.datetime.strptime(date_time_string, "%Y-%m-%dT%H:%M:%SZ")
 
 
-class Issue(object):
+class BaseIssue(object):
     """Represents a GitHub issue.
 
     Pull requests are also considered issues for most purposes, so this can
     represent pull requests as well.
     """
 
-    CONTROLLED_LABELS = {"idle"}
+    CONTROLLED_LABELS = {"idle", "waiting for submitter"}
 
     def __init__(self, repo_id, issue_number):
         self.repo_id = repo_id
@@ -79,18 +79,13 @@ class Issue(object):
     def fetch_labels(self):
         return call_github_api(self.get_base_url() + "/labels")
 
-    def get_idle_at(self):
-        comments = self.fetch_comments()
+    def fetch_controlled_label_names(self):
+        """Fetches the labels associated with the issue.
 
-        # If any contributor has commented on this issue, than it has been
-        # implicitly accepted and will never become idle.
-        if any(is_contributer(self.repo_id, comment["user"]["login"])
-               for comment in comments):
-            return None
-
-        # Otherwise, it'll expire a set time after the issue was created
-        issue_data = self.fetch_issue_data()
-        return convert_date_time(issue_data["created_at"]) + datetime.timedelta(days=7)
+        Returns only the names of controlled labels in a set.
+        """
+        return {i["name"] for i in self.fetch_labels()
+                if i["name"] in self.CONTROLLED_LABELS}
 
     def set_labels(self, labels):
         if not labels.issubset(self.CONTROLLED_LABELS):
@@ -98,8 +93,7 @@ class Issue(object):
                 "Uncontrolled labels present: %r" % (
                     labels - self.CONTROLLED_LABELS))
 
-        current_labels = {i["name"] for i in self.fetch_labels()
-                          if i["name"] in self.CONTROLLED_LABELS}
+        current_labels = self.fetch_controlled_label_names()
 
         to_delete = current_labels - labels
         for label in to_delete:
@@ -113,13 +107,86 @@ class Issue(object):
                         method=urlfetch.POST, payload=list(to_add))
 
 
+class Issue(BaseIssue):
+    def __init__(self, *args, **kwargs):
+        super(Issue, self).__init__(*args, **kwargs)
+
+    def get_idle_at(self):
+        """Get the time at which this issue will become idle.
+
+        If this time is in the past, then this issue is currently idle.
+
+        An issue is idle if a contributer has not commented on it within a
+        configurable time span (defaults to 7 days).
+        """
+
+        comments = self.fetch_comments()
+
+        # If any contributor has commented on this issue, than it has been
+        # implicitly accepted and will never become idle.
+        if any(is_contributer(self.repo_id, comment["user"]["login"])
+               for comment in comments):
+            return None
+
+        # Otherwise, it'll expire a set time after the issue was created
+        issue_data = self.fetch_issue_data()
+        return convert_date_time(issue_data["created_at"]) + datetime.timedelta(days=7)
+
+
+class PullRequest(BaseIssue):
+    def __init__(self, *args, **kwargs):
+        super(PullRequest, self).__init__(*args, **kwargs)
+
+    def get_idle_at(self):
+        """Gets the time at which this pull request will become idle.
+
+        If this time is in the past, then this pull request is currently idle.
+
+        To define what it takes for a PR to be idle... The amount of time it
+        takes for a PR to become idle is configurable and let's say that time
+        delta is `idle_time_delta`. An issue is idle if neither of these two
+        events have occurred within the last `idle_time_delta`: (1) a
+        contributor has commented, or (2) the label `waiting for submitter` has
+        been removed. A PR cannot be idle if the `waiting for submitter` label
+        is currently atttached to it.
+        """
+        # Figure out if we're waiting for the submitter (in which case we
+        # cannot become idle).
+        current_labels = self.fetch_controlled_label_names()
+        if "waiting for submitter" in current_labels:
+            return None
+
+        def get_latest(sequence):
+            pruned_sequence = [i for i in sequence if i is not None]
+            if not pruned_sequence:
+                return None
+
+            return max(pruned_sequence)
+
+        # Figure out that last time the "waiting for submitter" label was removed
+        # from this PR.
+        events = self.fetch_event_activity()
+        last_unlabeled = get_latest(
+            convert_date_time(event["created_at"]) for event in events
+            if event["event"] == "unlabeled" and
+               event["label"]["name"] == "waiting for submitter")
+
+        # Figure out the last time a contributor commented on this PR
+        comments = self.fetch_comments()
+        last_commented = get_latest(
+            convert_date_time(comment["created_at"]) for comment in comments
+            if is_contributer(self.repo_id, comment["user"]["login"]))
+
+        created_at = convert_date_time(self.fetch_issue_data()["created_at"])
+        return get_latest([created_at, last_unlabeled, last_commented]) + datetime.timedelta(days=7)
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
 
-        test_issue = Issue(RepoID("brownhead", "haunted-house"), 1)
-        test_issue.set_labels(set())
-        self.response.write("Hello")
+        test_pull_request = PullRequest(RepoID("brownhead", "haunted-house"), 2)
+        self.response.write(test_pull_request.get_idle_at().strftime("%Y-%m-%d %H:%M:%S"))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
