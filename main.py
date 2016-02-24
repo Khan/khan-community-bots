124f5b97d2a96c5196c9146aa6fd969233b7c80d
brownhead <johnsullivan.pem@gmail.com>
2016-02-24 13:26:16 -0800

    Determine when issue becomes idle.
    Test Plan:
    Hit the / endpoint and see a date 7 days after the creation date of
    https://github.com/brownhead/haunted-house/issues/1.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import urlparse
 import collections
 import json
 import logging
+import datetime
 
 import google.appengine.api.urlfetch as urlfetch
 import webapp2
@@ -17,6 +18,14 @@ def call_github_api(url):
     return json.loads(response.content)
 
 
+def is_contributer(repo_id, user_login):
+    return True
+
+
+def convert_date_time(date_time_string):
+    return datetime.datetime.strptime(date_time_string, "%Y-%m-%dT%H:%M:%SZ")
+
+
 class Issue(object):
     """Represents a GitHub issue.
 
@@ -24,29 +33,52 @@ class Issue(object):
     represent pull requests as well.
     """
 
-    def __init__(self, repo_id, issue_number, event_activity=None):
+    def __init__(self, repo_id, issue_number):
         self.repo_id = repo_id
         self.issue_number = issue_number
-        self.event_activity = event_activity
 
-    def fetch_event_activity(self, ignore_stored_value=False):
-        if ignore_stored_value and self.event_activity is not None:
-            return self.event_activity
+    def fetch_issue_data(self):
+        url = ("/repos/{repo_id.owner}/{repo_id.name}/issues/"
+               "{issue_number}").format(
+            repo_id=self.repo_id,
+            issue_number=self.issue_number)
 
+        return call_github_api(url)
+
+    def fetch_event_activity(self):
         url = ("/repos/{repo_id.owner}/{repo_id.name}/issues/{issue_number}"
                "/events").format(
             repo_id=self.repo_id,
             issue_number=self.issue_number)
 
-        self.event_activity = call_github_api(url)
-        return self.event_activity
+        return call_github_api(url)
+
+    def fetch_comments(self):
+        url = ("/repos/{repo_id.owner}/{repo_id.name}/issues/{issue_number}"
+               "/comments").format(
+            repo_id=self.repo_id,
+            issue_number=self.issue_number)
+
+        return call_github_api(url)
+
+    def get_idle_at(self):
+        comments = self.fetch_comments()
+
+        # If any contributor has commented on this issue, than it has been
+        # implicitly accepted and will never become idle.
+        if any(is_contributer(self.repo_id, comment["user"]["login"])
+               for comment in comments):
+            return None
+
+        # Otherwise, it'll expire a set time after the issue was created
+        issue_data = self.fetch_issue_data()
+        return convert_date_time(issue_data["created_at"]) + datetime.timedelta(days=7)
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        Issue(RepoID("brownhead", "haunted-house"), 1).fetch_event_activity()
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write(repr(Issue(RepoID("brownhead", "haunted-house"), 1).get_idle_at()))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
